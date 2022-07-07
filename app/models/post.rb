4b2567747cc533753e22e1a9f1b9e3c766ce31b3
moise <moisemlg90@gmail.com>
2022-07-07 19:52:20 +0200

    Create an update post count and a recent comment methods in posts table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
+  def posts_count_updated
+    users.increment!(:posts_counter)
+  end
   
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+ 
 end

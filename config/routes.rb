d4e318456f36edc1d236b57c1953119f1db7a3c5
moise <moisemlg90@gmail.com>
2022-07-11 15:58:53 +0200

    Create route for the users url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users do
+    resources :posts
+    resources :likes
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end

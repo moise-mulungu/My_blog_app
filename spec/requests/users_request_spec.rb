a0a9e80e36aa8d0fa4bba21016a6c72de21ba5b4
moise <moisemlg90@gmail.com>
2022-07-12 11:06:25 +0200

    Add rspec in users request

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+    describe 'GET /#index' do
+    before(:example) { get users_path } 
+        it 'returns http success' do
+            expect(response).to have_http_status(:success)
+        end
+        it "renders 'index' template" do
+            expect(response).to render_template('index')
+        end
+    end
+
+    describe 'GET /#show' do
+        before(:context) { get user_path[:id] } 
+        it 'returns http success' do
+            expect(response).to have_http_status(:success)
+        end
+        it "renders 'show' template" do
+            expect(response).to render_template('show')
+        end
+        it "assigns @user" do
+            expect(assigns(:user)).to eq(@user)
+        end
+        it 'signs in the user' do
+            expect(session[:user_id]).to eq(@user.id)
+        end
+        
+    end
 end

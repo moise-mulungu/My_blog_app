ff2dc9705689fcf29e5429490bdf92bf9035343f
moise <moisemlg90@gmail.com>
2022-07-12 11:06:58 +0200

    Add rspec for posts request

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,31 +1,31 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Posts', type: :request do
-#     describe 'GET /#index' do
-#         before(:example) { get posts_path } 
-#             it 'returns http success' do
-#                 expect(response).to have_http_status(:success)
-#             end
-#             it "renders 'index' template" do
-#                 expect(response).to render_template('index')
-#             end
-#         end
+RSpec.describe 'Posts', type: :request do
+    describe 'GET /#index' do
+        before(:example) { get posts_path } 
+            it 'returns http success' do
+                expect(response).to have_http_status(:success)
+            end
+            it "renders 'index' template" do
+                expect(response).to render_template('index')
+            end
+        end
     
-#         describe 'GET /#show' do
-#             before(:context) { get post_path[:id] } 
-#             it 'returns http success' do
-#                 expect(response).to have_http_status(:success)
-#             end
-#             it "renders 'show' template" do
-#                 expect(response).to render_template('show')
-#             end
-#             it "assigns @post" do
-#                 expect(assigns(:post)).to eq(@post)
-#             end
-#             it 'signs in the user' do
-#                 expect(session[:user_id]).to eq(@user.id)
-#             end
+        describe 'GET /#show' do
+            before(:context) { get post_path[:id] } 
+            it 'returns http success' do
+                expect(response).to have_http_status(:success)
+            end
+            it "renders 'show' template" do
+                expect(response).to render_template('show')
+            end
+            it "assigns @post" do
+                expect(assigns(:post)).to eq(@post)
+            end
+            it 'signs in the user' do
+                expect(session[:user_id]).to eq(@user.id)
+            end
             
-#         end
-#     end
-# end
+        end
+    end
+end

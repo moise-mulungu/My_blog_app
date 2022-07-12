60ef393d4d6231ffbd6290a9a98f889b2a5e6410
moise <moisemlg90@gmail.com>
2022-07-12 14:47:54 +0200

    Add new testing in users controller

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,31 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-    describe 'GET /#index' do
-        before(:example) { get posts_path } 
-            it 'returns http success' do
-                expect(response).to have_http_status(:success)
-            end
-            it "renders 'index' template" do
-                expect(response).to render_template('index')
-            end
-        end
-    
-        describe 'GET /#show' do
-            before(:context) { get post_path[:id] } 
-            it 'returns http success' do
-                expect(response).to have_http_status(:success)
-            end
-            it "renders 'show' template" do
-                expect(response).to render_template('show')
-            end
-            it "assigns @post" do
-                expect(assigns(:post)).to eq(@post)
-            end
-            it 'signs in the user' do
-                expect(session[:user_id]).to eq(@user.id)
-            end
-            
-        end
+  describe 'GET /#index' do
+    before(:context) { get post_path }
+    it 'is success' do
+      expect(response).to have_http_status(:success)
     end
+    it "renders 'index' template" do
+      expect(response).to render_template('Here is a list of all posts, comments, and likes for a given user')
+    end
+    it 'renders text with posts' do
+      expect(page).to have_text()
+    end
+  end
+
+  describe 'GET /#show' do
+    before(:context) { get post_path }
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+    it 'renders text with post' do
+      expect(page).to have_text('Here is a list of all posts, comments, and likes for a given user')
+    end
+    
+  end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-    describe 'GET /#index' do
-    before(:example) { get users_path } 
-        it 'returns http success' do
-            expect(response).to have_http_status(:success)
-        end
-        it "renders 'index' template" do
-            expect(response).to render_template('index')
-        end
+  describe 'GET /index' do
+    before(:example) { get '/users' }
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
     end
+    it "renders users index view" do
+      expect(response).to render_template(:index)
+    end
+    it 'shows content in the view' do
+      expect(response.body).to include('Here is a list of all posts, comments, and likes for a given user')
+    end
+  end
 
-    describe 'GET /#show' do
-        before(:context) { get user_path[:id] } 
-        it 'returns http success' do
-            expect(response).to have_http_status(:success)
-        end
-        it "renders 'show' template" do
-            expect(response).to render_template('show')
-        end
-        it "assigns @user" do
-            expect(assigns(:user)).to eq(@user)
-        end
-        it 'signs in the user' do
-            expect(session[:user_id]).to eq(@user.id)
-        end
-        
+  describe 'GET /show' do
+    before(:context) { get 'users/1' }
+  
+    it 'is success' do
+      expect(response).to have_http_status(:success)
     end
+    it "renders users show view" do
+      expect(response).to render_template(show:) 
+    end
+    it 'shows content in the view' do
+      expect(response.body).to include('Here is a list of all posts, comments, and likes for a given user')
+    end
+  end
 end

7eefa6387a31c41d6ca8fb789529e4756f0c8c8e
moise <moisemlg90@gmail.com>
2022-07-15 09:19:10 +0200

    Create methods in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @author = User.find(params[:user_id])
+    @post = @author.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_path(id: @post.author_id)
+    else
+      render :new, alert: "Post failed to save."
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end

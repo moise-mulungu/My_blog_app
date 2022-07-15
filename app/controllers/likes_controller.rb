4dd3ad5a209970c7661b859fa0e9fcffd55a30a7
moise <moisemlg90@gmail.com>
2022-07-15 09:13:45 +0200

    Create methods in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+    def new
+        @like = Like.new
+    end
+
+    def create
+        @like = Like.new(like_params)
+        @post = Post.find(params[:post_id])
+        @like = @post.likes.new(author_id: current_user.id, post_id: params[:post_id])
+    
+        if @like.save
+            redirect_to user_posts_path(user_id: @post.author_id, id: @post.id)
+        else
+            render :new, alert: "Like failed to save."
+        end
+    end
 end

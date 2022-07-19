e82ffa52c523410cc6cc7103e69555755c34a4e9
moise <moisemlg90@gmail.com>
2022-07-19 20:38:08 +0200

    Add integration specs for post show page

--- spec/feature/post_show_spec.rb
+++ spec/feature/post_show_spec.rb
@@ -2,45 +2,44 @@ require 'rails_helper'
 
 RSpec.describe 'Testing that user show page', type: :feature do
   before(:each) do
-    @user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer.',
-                        post_counter: 3)
+    @user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer.',  post_counter: 3)
 
-    @post = Post.create(title: 'First post', text: 'This is my first post', comment_counter: 2, likes_counter: 0,
-                        author: @user)
-
+    @post = Post.create(title:'First post', text: 'This is my first post', comment_counter: 0, likes_counter: 0, author: @user)
     @comment = Comment.create(text: 'Keep it up!', author: User.first, post: Post.first)
 
-    visit user_posts_path(user_id: @user.id)
+    visit user_post_path(user_id: @post.author_id, id: @post.id)
   end
 
-  it 'Shows post title.' do
-    expect(page).to have_content (@post.title)
+  it 'shows post title' do 
+    expect(page).to have_content @post.title
   end
 
-  it 'Show post name' do
-    expect(page).to have_content (@post.author.name)
+  it 'shows who wrote the post' do 
+    expect(page).to have_content @post.author.name
   end
 
-  it 'Show the number of posts each user has written' do
-    expect(page).to have_content @user.post_counter
+  it 'shows how many comments a post has' do 
+    expect(page).to have_content @post.comment_counter
   end
 
-  it 'Show how many comments a post has.' do
-    expect(page).to have_content (@post.comment_counter = 2)
+  it 'shows how many likes a post has' do 
+    expect(page).to have_content @post.likes_counter
   end
 
-  it 'Show how many likes a post has.' do
-    post = Post.first
-    expect(page).to have_content post.likes_counter
+  it 'shows post body' do
+    expect(page).to have_content @post.text 
   end
 
-  it 'can see the username of each commentor.' do
+  it 'shows user name of each comment' do 
     post = Post.first
     comment = post.comments.first
-    expect(page).to have_content(comment.author.name)
+    expect(page).to have_content comment.author.name
   end
 
-  it 'can see the comments of each commentor.' do
-    expect(page).to have_content 'Keep it up!'
+  it 'shows the comment each commenter left' do 
+    post = Post.first
+    comment = post.comments.first
+    expect(page).to have_content comment.text
   end
+  
 end

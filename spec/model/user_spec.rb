d73dff68a8b7761f0cd423150f79c7266898f9ec
moise <moisemlg90@gmail.com>
2022-07-21 22:53:44 +0200

    Update linters

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,7 +1,8 @@
 # require 'rails_helper'
 
 # RSpec.describe User, type: :model do
-#   author = User.new(name: 'John Doe', photo: 'https://example.com/photo.jpg', bio: 'he was born in 1990 in England', post_counter: 15)
+#   author = User.new(name: 'John Doe', photo: 'https://example.com/photo.jpg',
+# bio: 'he was born in 1990 in England', post_counter: 15)
 
 #   before { author.save }
 
@@ -46,7 +47,8 @@
 #   describe 'Validations for the post model' do
 #     before(:each) do
 #       @user = User.new(name: 'Tom', bio: 'I am a developer', posts_counter: 0)
-#       @post = Post.new(author: @user, title: 'This is a post', text: 'This is a post text', likes_counter: 0, comment_counter: 0)
+#       @post = Post.new(author: @user, title: 'This is a post', text: 'This is a post text',
+# likes_counter: 0, comment_counter: 0)
 #     end
 #   end
 # end
